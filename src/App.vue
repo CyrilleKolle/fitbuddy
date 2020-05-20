753643e7272c007c250c1696c63831c1a1775b5e
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-05-20 17:22:08 +0200

    done the home/feed page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/profile">Profile</router-link>
     </div>
+      <nav id="log">
+      <button>login</button>|
+      <button>register</button>|
+      <button>logout</button>
+    </nav>
     <router-view/>
+  
   </div>
 </template>
 
@@ -16,9 +23,37 @@
   text-align: center;
   color: #2c3e50;
 }
-
+#main-title{
+  font-family: 'Miller Ban It';
+  font-size: 2rem;
+}
+#log{
+  float: right;
+  position: relative;
+}
 #nav {
   padding: 30px;
+  position: absolute;
+}
+#feedId{
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#post{
+
+border-top: 1px solid #eee;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: auto;
+	padding-bottom: 30px;
+	padding-top: 40px;
+	text-align: center;
+  /* width: 600px;
+  height: 150px;
+  margin: 2px;
+  border: 1ex solid rgb(14, 7, 2); */
 }
 
 #nav a {
@@ -28,5 +63,39 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+#event-name{
+  /* float:left */
+  font-family: 'Miller Ban It';
+	font-size: 24px;
+	font-style: italic;
+	margin-bottom: 20px;
+  
+}
+#event-des{
+/* border: 0.2ex solid black; */
+	display: inline-block;
+	margin-left: auto;
+	margin-right: auto;
+}
+#event-time{
+	float: right;
+	margin-left: 15px;
+	margin-right: 15px; 
+  
+ 
+}
+#event-date{
+	float: right;
+	margin-left: 15px;
+	margin-right: 15px; 
+}
+#more-info{
+  display: inline-block;
+	margin-left: auto;
+	margin-right: auto;
+  color: blue;
+  float: right;
+  
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld id="main-title" msg="FitBuddy" />
+    <div id="feedId">
+      <div v-for="event in events" :key="event.id" id="post">
+        <span id="event-name">{{event.name}}</span>
+        <br>
+        <span id="event-des">{{event.description}}</span>
+        <br>
+        <span id="event-time">{{event.time}}</span>
+        <br>
+        <span id="event-date">{{event.date}}</span>
+      
+        <p id="more-info">...more</p>
+        </div>
+    </div>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  computed: {
+    events() {
+      return this.$store.state.events;
+    }
   }
-}
+};
 </script>
