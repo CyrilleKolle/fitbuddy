29c85519e17386711aaeef858793ee2202b03b2a
melkerlilja <melker.c.lilja@gmail.com>
2020-05-28 10:52:27 +0200

    Frontend profilsida, routerlink gjord till sidan

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   },
   {
     path: '/about',
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ProfileComp></ProfileComp>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProfileComp from "@/components/ProfileComp.vue";
+
+export default {
+  name: "Profile",
+  components: {
+    ProfileComp
+  }
+};
+</script>
