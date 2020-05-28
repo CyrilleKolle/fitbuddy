1e4ae0c67c3ec1fd046b24da8ee2da3cbe17cc9d
johannesryberg <johannes.ryberg@gmail.com>
2020-05-28 12:19:32 +0200

    'post-event' frontend färdig, saknar tid och kalender funktion

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/post">About</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Post from '../views/postEventView.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,10 +15,12 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( '../views/About.vue')
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    component: Post
   }
 ]
 
