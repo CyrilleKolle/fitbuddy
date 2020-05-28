b4551b70270adb0ad1d64c4f0157e4c18ba64bd2
Roman <Divkovicroman@gmail.com>
2020-05-28 12:06:44 +0200

    Registreringssida klar med komponent

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/signUp">Sign Up</router-link>
+
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from '../views/Registration.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/signUp',
+    name: 'SignUp',
+    component: SignUp
+  },
 ]
 
 const router = new VueRouter({
