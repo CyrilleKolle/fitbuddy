7ffb90d3c227573ca08033af248fac2c699368f4
melkerlilja <melker.c.lilja@gmail.com>
2020-05-29 13:52:56 +0200

    små ändringar i profil

--- src/components/ProfileComp.vue
+++ src/components/ProfileComp.vue
@@ -1,42 +1,50 @@
 <template>
-  <div>
-    <img
-      alt="Ladda upp bild på dig själv"
-      src="https://www.alzforum.org/sites/default/files/member-default.jpg"
-      style="border-radius: 50%; width: 200px; height:200px;"
-    />
+  <div class="profile">
+    <div class="img">
+      <img
+        alt="Ladda upp bild på dig själv"
+        src="https://www.alzforum.org/sites/default/files/member-default.jpg"
+        style="border-radius: 50%; width: 200px; height:200px;"
+      />
+      <br />
+      <b-button @click="clickMe" class="is-twitter">Välj en bild</b-button>
+    </div>
+    <div class="namediv">
+      <p>
+        Namn:
+        <input type="text" placeholder="Skriv ditt namn" />
+      </p>
+    </div>
+    <div class="agediv">
+      <p>
+        Ålder:
+        <input type="text" placeholder="Skriv in din ålder" />
+      </p>
+    </div>
+    <div class="genderdiv">
+      <!-- Här ska det vara att välja kön -->
+    </div>
+    <div class="citydiv">
+      <p>
+        Stad:
+        <select name="stad" id="stad">
+          <option value="gbg">Göteborg</option>
+          <option value="sthlm">Stockholm</option>
+          <option value="malmo">Malmö</option>
+        </select>
+      </p>
+    </div>
     <br />
-    <input type="button" value="Välj en bild" />
-    <p>
-      Namn:
-      <input type="text" placeholder="Skriv ditt namn" />
-    </p>
-    <p>
-      Ålder:
-      <input type="text" placeholder="Skriv in din ålder" />
-    </p>
-    <p>
-      Kön: Man
-      <input type="checkbox" />
-      Kvinna
-      <input type="checkbox" />
-      Annat:
-      <input type="checkbox" />
-    </p>
-    <p>
-      Stad:
-      <select name="stad" id="stad">
-        <option value="gbg">Göteborg</option>
-        <option value="sthlm">Stockholm</option>
-        <option value="malmo">Malmö</option>
-      </select>
-    </p>
-    <br />
-    <input type="button" value="Spara" />
+    <b-button class="is-twitter">Spara</b-button>
   </div>
 </template>
+
 <script>
 export default {
-  name: "Profile"
+  data() {
+    return {
+      checkboxGroup: ["flint"]
+    };
+  }
 };
 </script>
