4123a8030219c70727170b92d778d0c3dd449b04
melkerlilja <melker.c.lilja@gmail.com>
2020-06-01 11:24:12 +0200

    Ändringar i profilsidan, scroll funktion

--- src/components/ProfileComp.vue
+++ src/components/ProfileComp.vue
@@ -1,51 +1,53 @@
 <template>
-  <div class="profile">
-    <div class="img">
-      <h1>Profil</h1>
-      <br />
-      <img
-        alt="Ladda upp bild på dig själv"
-        src="https://www.alzforum.org/sites/default/files/member-default.jpg"
-        style="border-radius: 50%; width: 300px; height:300px;"
-      />
-      <br />
-      <b-button class="is-twitter">Välj en bild</b-button>
-    </div>
-    <div class="inputdiv">
-      <b-field label="Namn"></b-field>
-      <input type="text" placeholder="Skriv in ditt namn" value />
-    </div>
-    <div class="inputdiv">
-      <b-field label="Ålder">
-        <input type="text" placeholder="Skriv in din ålder" value />
-      </b-field>
-    </div>
-    <div class="genderdiv">
-      <section>
-        <b-field label="Kön">
-          <div class="block">
-            <b-radio v-model="gender" name="gender" native-value="Man">Man</b-radio>
-            <b-radio v-model="gender" name="gender" native-value="Kvinna">Kvinna</b-radio>
-            <b-radio v-model="gender" name="gender" native-value="Annat">Annat</b-radio>
-          </div>
+  <div class="fixedBox">
+    <div class="profile">
+      <div class="img">
+        <h1>Profil</h1>
+        <br />
+        <img
+          alt="Ladda upp bild på dig själv"
+          src="https://www.alzforum.org/sites/default/files/member-default.jpg"
+          style="border-radius: 50%; width: 300px; height:300px;"
+        />
+        <br />
+        <b-button class="is-twitter">Välj en bild</b-button>
+      </div>
+      <div class="inputdiv">
+        <b-field label="Namn"></b-field>
+        <input type="text" placeholder="Skriv in ditt namn" value />
+      </div>
+      <div class="inputdiv">
+        <b-field label="Ålder">
+          <input type="text" placeholder="Skriv in din ålder" value />
         </b-field>
-      </section>
+      </div>
+      <div class="genderdiv">
+        <section>
+          <b-field label="Kön">
+            <div class="block">
+              <b-radio v-model="gender" name="gender" native-value="Man">Man</b-radio>
+              <b-radio v-model="gender" name="gender" native-value="Kvinna">Kvinna</b-radio>
+              <b-radio v-model="gender" name="gender" native-value="Annat">Annat</b-radio>
+            </div>
+          </b-field>
+        </section>
+      </div>
+      <div class="citydiv">
+        <section>
+          <b-field label="Stad">
+            <b-select placeholder="Välj vilken stad">
+              <option value="gbg">Göteborg</option>
+              <option value="sthlm">Stockholm</option>
+              <option value="malmo">Malmö</option>
+            </b-select>
+          </b-field>
+        </section>
+      </div>
+      <br />
+      <b-button class="is-twitter">Spara</b-button>
+      <br />
+      <br />
     </div>
-    <div class="citydiv">
-      <section>
-        <b-field label="Stad">
-          <b-select placeholder="Välj vilken stad">
-            <option value="gbg">Göteborg</option>
-            <option value="sthlm">Stockholm</option>
-            <option value="malmo">Malmö</option>
-          </b-select>
-        </b-field>
-      </section>
-    </div>
-    <br />
-    <b-button class="is-twitter">Spara</b-button>
-    <br />
-    <br />
   </div>
 </template>
 
@@ -60,6 +62,26 @@ export default {
 </script>
 
 <style scoped>
+.fixedBox {
+  box-shadow: 1px 1px 30px 0 rgba(0, 0, 0, 0.7);
+  width: 85%;
+  left: 0;
+  right: 0;
+  max-width: 1000px;
+  padding: 30px 40px;
+  margin: 0 auto;
+  position: fixed;
+  align-content: center;
+}
+.profile {
+  overflow-y: auto;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  scrollbar-width: none;
+}
 h1 {
   font-size: 2.5em;
   left: 0;
